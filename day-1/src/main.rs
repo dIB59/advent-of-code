1a23d3b55bebb2e94efd8f1bb9609c96a11017a2
Ibrahim <Ibrahim@ebs.net.pk>
2023-12-03 14:35:36 +0100

    removed logic from main file

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,46 +1,8 @@
+mod counter;
+
 use std::io;
+use crate::counter::{Counter, SimpleParenthesesCounter, SimplePositionCounter};
 
-pub trait Counter {
-    fn count_char(&self, input: &str) -> i32;
-}
-
-pub struct SimpleParenthesesCounter;
-pub struct SimplePositionCounter;
-
-impl Counter for SimpleParenthesesCounter {
-    fn count_char(&self, input: &str) -> i32 {
-        let mut count: i32 = 0;
-        for c in input.chars() {
-            if c.eq_ignore_ascii_case(&'(') {
-                count += 1;
-            }
-            if c.eq_ignore_ascii_case(&')') {
-                count -= 1;
-            }
-        }
-        count
-    }
-}
-
-impl Counter for SimplePositionCounter {
-    fn count_char(&self, input: &str) -> i32 {
-        let mut position: i32 = 0;
-        let mut count: i32 = 0;
-        for c in input.chars() {
-            if c.eq_ignore_ascii_case(&'(') {
-                count += 1;
-            }
-            if c.eq_ignore_ascii_case(&')') {
-                count -= 1;
-            }
-            position += 1;
-            if count.is_negative() {
-                return position;
-            }
-        }
-        -1
-    }
-}
 
 fn main() {
     let mut puzzle_input = String::new();
