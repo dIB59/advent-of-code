fb69eb070cba8805c2eda0e5d9ba67f51b2c3119
Ibrahim <Ibrahim@ebs.net.pk>
2023-12-04 00:40:47 +0100

    created methods

---
+++ day-1/src/reader.rs
@@ -0,0 +1,33 @@
+pub trait Reader {
+    fn read(input: &str) -> Vec<i32>;
+}
+
+pub struct FirstAndLastInteger;
+
+impl Reader for FirstAndLastInteger {
+    fn read(input: &str) -> Vec<i32> {
+        let mut numbers = Vec::new();
+        for line in input.lines() {
+            let chars: Vec<char> = line.chars()
+                .filter(|c| c.is_numeric())
+                .collect();
+            Self::first_last_ints(&mut numbers, chars);
+        }
+        return numbers
+    }
+}
+
+impl FirstAndLastInteger {
+    fn first_last_ints(numbers: &mut Vec<i32>, chars: Vec<char>) {
+        let mut some: String = String::from("");
+
+
+        if let Some(c) = chars.first() {
+            some.push(*c);
+        }
+        if let Some(c) = chars.last() {
+            some.push(*c);
+        }
+        numbers.push(some.parse::<i32>().unwrap_or(0));
+    }
+}
